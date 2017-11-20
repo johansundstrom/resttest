62a399c44e37d4ebcb58ec44a03e8e3d17cf69df
Johan sundstrom <johansundstrom@users.noreply.github.com>
2017-11-20 11:53:33 +0100

    Update controller.js
    Städat

--- api/controllers/controller.js
+++ api/controllers/controller.js
@@ -13,8 +13,6 @@ exports.list_all_tasks = function(req, res) {
 };
 
 
-
-
 exports.create_a_task = function(req, res) {
   var new_task = new Task(req.body);
   new_task.save(function(err, task) {
@@ -44,8 +42,6 @@ exports.update_a_task = function(req, res) {
 
 
 exports.delete_a_task = function(req, res) {
-
-
   Task.remove({
     _id: req.params.taskId
   }, function(err, task) {
