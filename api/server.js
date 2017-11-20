6d8daec528e01f159e7bcb849bda953e5f963e9f
Johan sundstrom <johansundstrom@users.noreply.github.com>
2017-11-20 15:46:35 +0100

    Update server.js
    Upd

--- api/server.js
+++ api/server.js
@@ -2,13 +2,12 @@ var express = require('express'),
   app = express(),
   port = process.env.PORT || 3000,
   mongoose = require('mongoose'),
-  Task = require('./api/models/model'), //created model loading here
+  Account = require('./api/models/model'), //created model loading here
   bodyParser = require('body-parser');
   
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/Tododb'); 
-
+mongoose.connect('mongodb://localhost/accountdb'); 
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
