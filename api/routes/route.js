11e4be5c33f7e40cfc45c8fa97f57df81d85e6fa
Johan sundstrom <johansundstrom@users.noreply.github.com>
2017-11-20 19:28:50 +0100

    Update route.js
    Upd

--- api/routes/route.js
+++ api/routes/route.js
@@ -3,12 +3,12 @@ module.exports = function(app) {
   var account = require('../controllers/Controller');
 
   // Account Routes
-  app.route('/account')
+  app.route('/accounts')
     .get(account.list_account)
     .post(account.create_account);
 
 
-  app.route('/account/:taskId')
+  app.route('/accounts/:taskId')
     .get(account.read_account)
     .put(account.update_account)
     .delete(account.delete_account);
